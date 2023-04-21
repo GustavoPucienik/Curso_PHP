88b95ce77938b0478e44492f0c9c7acb85e5eb9f
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-04-21 20:00:22 -0300

    manipulação de string

---
+++ ex003/index0.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Exemplo de php</h1>
+    <?php
+    //const PALAVRA = "mundo";
+    //echo "Ola " . PALAVRA;
+
+    //echo "hoje é dia " . date("d")
+
+    //$nom = "Rodrigo";
+    //$snom = "Nogueira";
+
+    //echo "$nom  \"Minotauro\" $snom";
+
+    ////HEREDOC
+    //$canal = "Curso em Vídeo";
+    //$ano = date('y');
+    //echo <<< FRASE 
+    //        Ola galera do $canal!
+    //        Como esta sendo o $ano de vocês?
+    //FRASE;
+
+    ////Nowdoc
+    //$canal = "Curso em Vídeo";
+    //$ano = date('y');
+    //echo <<< 'FRASE '
+    //        Ola galera do $canal!
+    //        Como esta sendo o $ano de vocês?
+    //FRASE;
+    ?>
+</body>
+</html>
+
+    
+    
