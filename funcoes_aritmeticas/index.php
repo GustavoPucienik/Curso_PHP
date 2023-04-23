564df9f962b0696c180f5b12ae5ce9378d84e4b6
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-04-22 22:18:19 -0300

    Funcoes aritmeticas

---
+++ funcoes_aritmeticas/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções aritméticas</title>
+</head>
+<body>
+    <?php 
+        //$num = abs(-13); //função abs() retorna o valor absoluto, retira o sinal
+        //echo $num;
+
+       // $num = base_convert(15, 10, 2); //converte o numero
+       // $num0 = base_convert(15, 10, 16); // numero, base do numero, base que num sera convertido
+       // echo "$num<br>$num0";
+
+       //ceil(), floor(), round()
+        // hypot()  Hipotenusa
+
+        //intdiv(), divisão inteira
+        //$r = intdiv(5, 2);
+        //print("A resposta é $r");
+        
+        //$r = min(5, 2, 1); // retorna menor valor
+        //print("A resposta é $r");
+
+        //$r = max(7, 5, 2, 1); // retorna maior valor
+        //print("A resposta é $r");
+
+        //$r = pi();// valor de pi
+        //print("A resposta é $r");
+
+        //pow(5, 2) potencia, com função pow() você perde a ordem de precendencia
+        //sqrt(81) raiz quadrada
+    ?>
+    
+</body>
+</html>
