725a03fc2c7851ad7e1141f35b61b580a564d838
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-04-23 12:15:19 -0300

    Desafio 1

---
+++ d001/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Titulo</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+        <section>
+    <form action="result.php" method="post">
+        <h1>Escolha um numero</h1>
+            <label for="numero">Numero</label>
+            <input type="number" name="numero" required>
+            <input type="submit" value="Mostrar">
+    </form>
+        </section>
+
+</body>
+</html>
---
+++ d001/result.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+    <?php 
+        $num = $_POST["numero"] ?? 0;
+        $ant = -1 + $num;
+        $suc = 1 + $num;
+    ?>
+<body>
+    <section>
+        <h1>Resultado final:</h1>
+       <?php echo "<p>Numero escolhido:  $num</p>
+                   <p>Numero antecessor:  $ant</p>
+                   <p>Numero sucessor:  $suc</p>
+       "?>
+        <button id="btn">Voltar</a></p>
+    </section>
+    
+</body>
+<script> 
+ const btnvoltar = document.getElementById("btn")
+ btnvoltar.addEventListener("click", function(){window.location.href='index.php'})
+</script>
+</html>
