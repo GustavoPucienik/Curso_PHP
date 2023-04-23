ab40b187f2c0c7d90b0b0760e6e7e1ceab237546
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-04-22 22:59:06 -0300

    Operadores aritmeticos

---
+++ operadores_aritmeticos/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores Aritméticos do PHP</title>
+</head>
+<body>
+    <h1>Operadores Aritméticos</h1>
+    <?php 
+        $r = "2" . "2";
+        $s = "2" + "2";
+        echo "concatena: $r <br>
+                Soma: $s ";
+
+              /*   + adição
+                - subtração
+                * multiplicação
+                / divisão real
+                % resto da divisao inteira
+                ** exponenciação
+                */
+    ?> 
+    
+</body>
+</html>
