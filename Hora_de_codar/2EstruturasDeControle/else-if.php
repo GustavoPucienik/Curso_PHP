f737b0e2145931d397ade9794144f587c89925ba
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-04-26 17:31:46 -0300

    Estrutura de controle

---
+++ Hora_de_codar/2EstruturasDeControle/else-if.php
@@ -0,0 +1,16 @@
+<?php 
+
+    if(10 > 5){
+        echo "entrou no if <br>";
+    } else if(10>6){
+        echo "Entrou no else if <br>";
+    }
+
+    if(10 < 5){
+        echo "Entrou no segundo if <br>";
+    } else if(10>6){
+        echo "Entrou mo else if  <br>";
+    } else if(10>6){
+        echo "Entrou mo else if 2 <br>";
+    }
+?>
