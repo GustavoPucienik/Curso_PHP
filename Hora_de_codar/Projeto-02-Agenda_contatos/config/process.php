13cda3425344db078293fbad1753a979134d7084
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-05-13 22:11:39 -0300

    criando a pagina inicial

--- Hora_de_codar/Projeto-02-Agenda_contatos/config/process.php
+++ Hora_de_codar/Projeto-02-Agenda_contatos/config/process.php
@@ -5,12 +5,17 @@
     include_once("conection.php");
     include_once("url.php");
 
+    $contacts = [];
+
     $query = "SELECT * FROM contacts";
 
     $stmt = $conn->prepare($query);
 
     $stmt->execute();
 
+
     $contacts = $stmt->fetchAll();
+
+    $conn = null;
 
 ?>
--- Hora_de_codar/Projeto-02-Agenda_contatos/index.php
+++ Hora_de_codar/Projeto-02-Agenda_contatos/index.php
@@ -1,8 +1,38 @@
 <?php 
     include_once("templates/header.php")
 ?>
-    <h1>Titulo</h1>
-    <i class="fas fa-eye"></i>
+    <div class="container">
+        <?php if(isset($printMsg) && $printMsg != ""): ?>
+            <p id="msg"><?php $printMsg?></p>
+            <?php endif; ?>
+            <h1 id="main-title">Minha agenda</h1>
+            <?php if(is_countable($contacts) && count($contacts) > 0): ?>
+                <table class="table" id="contacts-table">
+                    <thead>
+                        <th scope="col">id</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Telefone</th>
+                        <th scope="col"></th>
+                    </thead>
+                    <tbody>
+                        <?php foreach($contacts as $contact): ?>
+                         <tr>
+                            <td scope="row"><?php $contact['id'] ?></td>
+                            <td scope="row"><?php $contact['name'] ?></td>
+                            <td scope="row"><?php $contact['phone'] ?></td>
+                            <td class="actions">
+                                <a href="#"><i class="fa fa-eye check-icon"></i></a>
+                                <a href="#"><i class="fa fa-edit edit-icon"></i></a>
+                                <button type="submit"><i class="fas fa-times delete-icon"></i></button>
+                            </td>
+                         </tr>   
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            <?php else: ?>
+                <p id="empty-list-text">Ainda não há contatos na sua agenda, <a href="<?= $BASE_URL?>create.php">Clique aqui para adicionar</a>.</p>
+            <?php endif; ?>
+    </div>
 <?php 
     include_once("templates/footer.php")
 ?>
