758797bff77e545334ee4a409049bb1091760f70
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-05-16 23:00:19 -0300

    Criando model User, e iniciando a autenticação

---
+++ Hora_de_codar/Projeto-03-Movie/auth_process.php
@@ -0,0 +1,36 @@
+<?php 
+
+require_once("globals.php");
+require_once("db.php");
+require_once("models/user.php");
+require_once("models/message.php");
+require_once("dao/userDAO.php");
+
+$message = new Message($BASE_URL);
+
+// resgata o tipo do formulario
+$type = filter_input(INPUT_POST, "type");
+
+// Verificação do tipo de formulario
+if($type === "register"){
+
+    $name = filter_input(INPUT_POST, "name");
+    $lastname = filter_input(INPUT_POST, "lastname");
+    $email = filter_input(INPUT_POST, "email");
+    $password = filter_input(INPUT_POST, "password");
+    $confirmpassword = filter_input(INPUT_POST, "confirmpassword");
+
+    //Verificação de dados
+    if ($name && $lastname && $email && $password) {
+
+
+        
+    } else {
+
+        //Enviar uma msg de erro, de dados faltantes
+        $message->setMessage("Por favor preencha todos os campos", "error", "back");
+
+    }
+
+} else if($type === "login"){}
+?>
