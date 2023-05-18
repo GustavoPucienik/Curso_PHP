6c24fe447ff609402647f8edc0d5aefceebd0167
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-05-18 18:11:56 -0300

    Fazendo a home

--- Hora_de_codar/Projeto-03-Movie/index.php
+++ Hora_de_codar/Projeto-03-Movie/index.php
@@ -2,7 +2,32 @@
     require_once("templates/header.php");
 ?>
     <div id="main-container" class='container-fluid'>
-        <h1>Corpo do site</h1>
+        <h2 class="section-title">Filmes novos</h2>
+        <p class="section-description">Veja as críticas dos últimos filmes adicionados no MovieStar.</p>
+        <div class="movies-container">
+            <div class="card movie-card">
+                <div class="card-image-top" style="background-image: url('<?= $BASE_URL?>img/movies/fundo.jpg')"></div>
+                <div class="card-body">
+                    <p class="card-rating">
+                        <i class="fas fa-star"></i>
+                        <span class="rating">9</span>
+                    </p>
+                    <h5 class="card-title">Titulo do filme</h5>
+                    <a href="#" class="btn btn-primary rate-btn">Avaliar</a>
+                    <a href="#" class="btn btn-primary card-btn">Conhecer</a>
+                </div>
+            </div>
+        </div>
+        <h2 class="section-title">Ação</h2>
+        <p class="section-description">Veja os melhores filmes de ação.</p>
+        <div class="movies-container">
+
+        </div>
+        <h2 class="section-title">Comédia</h2>
+        <p class="section-description">Veja os melhores filmes de comédia.</p>
+        <div class="movies-container">
+
+        </div>
     </div>
 
 <?php 
